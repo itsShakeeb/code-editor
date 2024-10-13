990e3c114f63891e2fff2635b5f1fdfa9ffd5d99
QL-Shakeeb-Arsalan <shakeeb.arsalan@quokkalabs.com>
2024-10-13 18:51:04 +0530

    added logger

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+  console.count("Request Received");
   res.send('Hello You"re getting response from server');
 });
 
