f26a8ab1db511ad6d5e415955b42305d707ce7f2
QL-Shakeeb-Arsalan <shakeeb.arsalan@quokkalabs.com>
2024-10-26 15:54:28 +0530

    nodejs image and port issue

--- routes/create-container.js
+++ routes/create-container.js
@@ -6,56 +6,49 @@ const getPort = require("get-port");
 const router = express.Router();
 
 const docker = new Docker();
+const nodeImage = "node:alpine";
 
 async function createContainer(req, res) {
   try {
     // Generate a unique name for each container (using uuid or Date)
     const containerName = `nodejs-container-${crypto.randomUUID()}`; // Unique container name
-    const appName = `react-app-${crypto.randomUUID()}`; //Unique react app name
+    // const appName = `react-app-${crypto.randomUUID()}`; //Unique react app name
 
     // const randomPort = 3001
     const randomPort = await getPort();
     const port = randomPort.toString();
 
     const container = await docker.createContainer({
-      Image: "node:alpine",
+      Image: nodeImage,
       Cmd: [
         "sh", // to execute shell command
         "-c", // arguments to tell docker to treat as single command after that
         // `npx create-react-app ${appName} && cd ${appName} && npm i && npm start -- --host 0.0.0.0 --port ${port}`,
-        `npx create-react-app my-app && cd my-app && npm i && npm start -- --host 0.0.0.0 --port 3000`,
+        // `npm install -g npm@9.8.1 && npm install -g create-react-app && create-react-app my-app && cd my-app && npm i && npm start -- --host 0.0.0.0 --port 3000`,
+        `npm install -g npm@9.8.1 && npm create vite@latest my-vite-react-app -- --template react && cd my-vite-react-app && npm i && npm run dev -- --host 0.0.0.0 --port 3000`,
       ],
       name: containerName,
-      WorkingDir: "/my-app",
-      // Tty: true,
       ExposedPorts: {
-        // [`${port}/tcp`]: {}, // React dev server runs on port port by default
         "3000/tcp": {}, // React dev server runs on port port by default
       },
       HostConfig: {
         PortBindings: {
-          // [`${port}/tcp`]: [{ HostPort: port }], // Bind random host port to container's port 3000
-          "3000/tcp": [{ HostPort: '18415' }], // Bind random host port to container's port 3000
+          "3000/tcp": [{ HostPort: port }], // Bind random host port to container's port 3000
         },
         RestartPolicy: {
-          Name: "always", // Restarts container on failure
+          Name: "always", // Restart the container if it crashes
         },
       },
     });
 
     // Start the container
     await container.start();
-    console.log("Container started:", container.id);
-
-    const containerInfo = await container.inspect();
-    const containerIP = containerInfo.NetworkSettings.IPAddress;
 
     return res.status(200).json({
       message: "Container started successfully!",
       containerId: container.id,
       containerName: containerName,
       exposedPort: randomPort,
-      appRunning: `http://${containerIP}:${randomPort}`,
     });
   } catch (err) {
     if (err.statusCode === 404) {
@@ -69,7 +62,8 @@ async function createContainer(req, res) {
 }
 
 async function pullImageAndCreateContainer(res) {
-  docker.pull("node:latest", (pullErr, stream) => {
+  docker.pull(nodeImage, (pullErr, stream) => {
+    console.log("Node.js image pulling");
     if (pullErr) {
       console.error("Error pulling the image:", pullErr);
       return res
@@ -86,7 +80,7 @@ async function pullImageAndCreateContainer(res) {
       }
 
       console.log("Image pulled successfully. Creating container...");
-      createContainer(res); // Retry container creation after pulling the image
+      createContainer(null, res); // Retry container creation after pulling the image
     }
 
     function onProgress(event) {
